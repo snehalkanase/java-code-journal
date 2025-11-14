881d51fc206c81eb92308e0e62d90749394e6e86
Snehal-Genxhire <snehal.kanase.genxhire@gmail.com>
2025-11-14 21:55:41 +0530

    Updated

--- Projects/RollingTwoDice/RollingDiceGui.java
+++ Projects/RollingTwoDice/RollingDiceGui.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.IntrospectionException;
 import java.util.Random;
 
 public class RollingDiceGui extends JFrame {
